3420db4e603c480faad138c1fd8b15c0a624bf16
Suraj Kewat <suraj.kewat@getastra.com>
2023-07-29 22:41:19 +0530

    intial commit

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,21 @@
+import { connectToDB } from '@utils/database'
+import Prompt from '@models/prompt'
+
+export const POST = async (req, res) => {
+    const { userId, prompt, tag } = await req.json()
+
+    try {
+        await connectToDB()
+        const newPromt = new Prompt({
+            creator: userId,
+            prompt: prompt,
+            tag: tag,
+        })
+
+        await newPromt.save()
+
+        return new Response(JSON.stringify(newPromt), { status: 201 })
+    } catch (error) {
+        new Response('Failed to create a new prompt', { status: 500 })
+    }
+}
